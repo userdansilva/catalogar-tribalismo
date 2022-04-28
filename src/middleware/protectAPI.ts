93fc2d6b09ac0e4c8eb7ddbfa2ca490874c43557
userdansilva <daniel.sousasilva97@outlook.com>
2022-04-28 14:46:01 -0300

    Teste com console.log

--- src/middleware/protectAPI.ts
+++ src/middleware/protectAPI.ts
@@ -4,6 +4,7 @@ export const protectAPI = (
   handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void | NextApiResponse<any>>
 ) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log(new URL(req.headers.referer as string).origin)
     if (new URL(req.headers.referer as string).origin !== process.env.DOMAIN) {
       return res.status(403).json({ success: false, message: `Forbidden` })
     }
