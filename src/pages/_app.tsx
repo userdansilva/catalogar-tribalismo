a689d47acc384942355462238c0e3d0b15b425db
Daniel Silva <daniel.sousasilva97@outlook.com>
2022-10-14 19:07:15 -0300

    Remoção do Google Analytics e Facebook Pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,11 @@ import type { AppProps } from 'next/app'
 
 import { DesignProvider } from '../hooks/useDesign'
 
-import { Analytics } from '../components/Analytics'
-
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
-      {process.env.NODE_ENV !== 'development' && <Analytics />}
       <DesignProvider>
         <Component {...pageProps} />
       </DesignProvider>
