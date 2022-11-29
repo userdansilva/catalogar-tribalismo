ddfab18b99c8bf2398ff936bc2c4dc91e51bb449
Daniel Silva <daniel.sousasilva97@outlook.com>
2022-11-29 19:52:27 -0300

    Google tags added!

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -3,19 +3,18 @@ import Script from 'next/script'
 
 export const Analytics = () => (
   <Fragment>
-    <Script src="https://www.googletagmanager.com/gtag/js?id=G-WNXGH9QP18" strategy="lazyOnload" />
+    {/* <Script src="https://www.googletagmanager.com/gtag/js?id=G-WNXGH9QP18" strategy="lazyOnload" /> */}
     <Script strategy="lazyOnload" id="google-tags">
       {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-WNXGH9QP18');
-        gtag('config', 'UA-124606827-3');
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K6PVMH8');
       `}
     </Script>
 
-    <Script strategy="lazyOnload" id="fc-pixel-tags">
+    {/* <Script strategy="lazyOnload" id="fc-pixel-tags">
       {`
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -28,9 +27,9 @@ export const Analytics = () => (
         fbq('init', '165638437257579');
         fbq('track', 'PageView');
       `}
-    </Script>
+    </Script> */}
 
-    <noscript
+    {/* <noscript
       dangerouslySetInnerHTML={{
         __html: `<img
         height="1"
@@ -44,6 +43,6 @@ export const Analytics = () => (
     <Script
       src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/7265390e-d6cb-481b-8148-9f158b48455d-loader.js"
       strategy="lazyOnload"
-    />
+    /> */}
   </Fragment>
 )
