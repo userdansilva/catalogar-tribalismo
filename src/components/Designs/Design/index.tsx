3a31be53f20c3a7174721febdcfd952670c7bd44
Daniel Silva <daniel.sousasilva97@outlook.com>
2024-11-05 22:00:09 -0300

    chore: carossel básico

--- src/components/Designs/Design/index.tsx
+++ src/components/Designs/Design/index.tsx
@@ -1,4 +1,15 @@
+/* eslint-disable react/no-array-index-key */
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/lib/shadcn/ui/carousel";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 import { FormattedDesign } from "../../../types/Design";
 
 interface DesignProps {
@@ -7,23 +18,63 @@ interface DesignProps {
 }
 
 export function Design({ data: design, onClick }: DesignProps) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const isMultiple = design.images.length > 1;
+
   return (
-    <button
-      type="button"
-      className="group relative cursor-pointer overflow-hidden rounded-2xl bg-slate-100 outline-none focus-visible:shadow-lg focus-visible:shadow-brand-400 focus-visible:ring-2 focus-visible:ring-brand-600 focus-visible:ring-offset-2"
-      onClick={() => onClick(design)}
-      tabIndex={0}
-    >
-      <Image src={design.images[0].webp} alt={design.title} width={600} height={600} />
-      <div className="absolute inset-0 bg-gradient-to-t from-slate-900 to-transparent opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
-      <div className="absolute -bottom-8 flex w-full flex-col gap-2 px-8 opacity-0 transition-all duration-300 ease-in-out group-hover:bottom-8 group-hover:opacity-100">
-        <span className="truncate text-lg text-white">{design.title}</span>
-        <span className="mr-auto rounded-lg bg-brand-600 px-2 py-1 font-semibold text-white">
-          ID:
-          {" "}
-          {design.id}
-        </span>
-      </div>
-    </button>
+    <Carousel className="group w-full overflow-hidden rounded-2xl bg-slate-100" setApi={setApi}>
+      <CarouselContent onClick={() => onClick(design)}>
+        <div className="absolute inset-0 bg-gradient-to-t from-slate-900 to-transparent opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
+        <div className="absolute -bottom-8 flex w-full flex-col gap-2 px-8 opacity-0 transition-all duration-300 ease-in-out group-hover:bottom-8 group-hover:opacity-100">
+          <span className="truncate text-lg text-white">{design.title}</span>
+          <span className="mr-auto rounded-lg bg-brand-600 px-2 py-1 font-semibold text-white">
+            {`ID: ${design.id}`}
+          </span>
+        </div>
+
+        {design.images.map((image) => (
+          <CarouselItem
+            key={image.id}
+          >
+            <Image src={image.webp} alt={design.title} width={600} height={600} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+
+      {isMultiple && (
+        <>
+          <CarouselPrevious className="left-2" />
+          <CarouselNext className="right-2" />
+
+          <div className="absolute inset-x-0 bottom-0 flex justify-center p-4">
+            <div className="flex space-x-2">
+              {api?.scrollSnapList().map((_, i) => (
+                <span
+                  key={i}
+                  className={cn(
+                    "block size-2 rounded-full bg-slate-300",
+                    (current === (i + 1)) && "bg-brand-600",
+                  )}
+                />
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </Carousel>
   );
 }
--- src/components/Modal/DesignDetailModal/index.tsx
+++ src/components/Modal/DesignDetailModal/index.tsx
@@ -1,11 +1,33 @@
+/* eslint-disable react/no-array-index-key */
 import Image from "next/image";
 import {
   CloseButton, Dialog, DialogBackdrop, DialogPanel, DialogTitle,
 } from "@headlessui/react";
 import { FiX } from "react-icons/fi";
+import { ButtonHTMLAttributes, useEffect, useState } from "react";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/lib/shadcn/ui/carousel";
+import { cn } from "@/lib/utils";
 import { FormattedDesign } from "../../../types/Design";
 import { getIconsByName } from "../../../utils/get-icon-by-name";
 
+function CloseButtonUI(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      type="button"
+      {...props}
+      className="absolute right-3 top-3 outline-brand-600 focus:text-brand-600 md:right-6 md:top-6"
+    >
+      <FiX size={24} />
+    </button>
+  );
+}
 interface DesignDetailModalProps {
   isOpen: boolean
   onClose: () => void
@@ -15,24 +37,64 @@ interface DesignDetailModalProps {
 export function DesignDetailModal({
   design, isOpen, onClose,
 }: DesignDetailModalProps) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const isMultiple = design.images.length > 1;
+
   return (
     <Dialog open={isOpen} onClose={onClose}>
       <DialogBackdrop className="fixed inset-0 bg-slate-900/30" />
 
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
         <DialogPanel className="relative mx-auto max-h-full max-w-[1120px] overflow-x-scroll rounded-xl bg-white p-10 lg:min-w-full xl:min-w-[1120px]">
-          <CloseButton as={() => (
-            <button
-              onClick={() => onClose()}
-              className="absolute right-3 top-3 outline-brand-600 focus:text-brand-600 md:right-6 md:top-6"
-            >
-              <FiX size={24} />
-            </button>
-          )}
-          />
+          <CloseButton as={CloseButtonUI} />
 
           <div className="flex flex-col gap-8 lg:flex-row">
-            <Image src={design.images[0].webp} alt={design.title} width={600} height={600} />
+            <Carousel className="group w-full max-w-[600px] overflow-hidden rounded-2xl bg-slate-100" setApi={setApi}>
+              <CarouselContent>
+                {design.images.map((image) => (
+                  <CarouselItem
+                    key={image.id}
+                  >
+                    <Image src={image.webp} alt={design.title} width={600} height={600} />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+
+              {isMultiple && (
+                <>
+                  <CarouselPrevious className="left-2" />
+                  <CarouselNext className="right-2" />
+
+                  <div className="absolute inset-x-0 bottom-0 flex justify-center p-4">
+                    <div className="flex space-x-2">
+                      {api?.scrollSnapList().map((_, i) => (
+                        <span
+                          key={i}
+                          className={cn(
+                            "block size-2 rounded-full bg-slate-300",
+                            (current === (i + 1)) && "bg-brand-600",
+                          )}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </>
+              )}
+            </Carousel>
 
             <div className="flex flex-1 flex-col gap-3 md:gap-6">
               <div className="flex flex-wrap gap-2">
